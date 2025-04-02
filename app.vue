12a089f4dd34b078b15959b008353e08272b959a
TintinLeGrand <ethan.leneindre9@gmail.com>
2025-04-02 11:26:28 +0200

    feat: Main page presentation

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="select-none max-w-screen font-mono">
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+import MountainCarousel from '@/components/montpic/MountainCarousel.vue'
+import { APP_NAME, APP_SLOGAN } from '@/constants/config'
+</script>
+
 <template>
-    <h1 class="text-lg font-extrabold">Bienvenue</h1>
-    <p>sur Montpic</p>
+    <div class="flex flex-col items-center justify-start text-center min-h-screen p-2 pt-10 gap-4">
+        <h1 class="text-6xl lg:text-9xl font-bold">{{ APP_NAME }}</h1>
+        <p class="text-2xl">{{ APP_SLOGAN }}</p>
+    </div>
+    <NuxtImg src="/mountain.webp" class="fixed w-screen -z-50 align-middle -bottom-1/12 sm:-bottom-1/5 md:-bottom-1/4 lg:-bottom-1/3 xl:-bottom-2/5 blur-sm brightness-75 dark:brightness-100" />
 </template>
